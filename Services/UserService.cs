b9b060c0772c40cdcd3d89f7842c13e3856772f3
Gustavo Araujo <travaluke2@gmail.com>
2026-05-01 01:38:53 -0300

    Expose UserService and add AddUser method

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AdminAuditApp.Models;
 
 namespace AdminAuditApp.Services
 {
-    internal class UserService
+    public class UserService
     {
+       private List<string> user = new List<string>();
+
+
+        public string AddUser(User user)
+        {
+            return 0.ToString();
+        }
     }
+
+
 }
