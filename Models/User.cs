d8f0be4b55f2f3391d9f5345251e38e739f9d7e7
Gustavo Araujo <travaluke2@gmail.com>
2026-05-01 01:28:56 -0300

    Make User class public and add core properties

--- Models/User.cs
+++ Models/User.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace AdminAuditApp.Models
 {
-    internal class User
+    public class User
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public bool IsActive { get; set; }
     }
 }
